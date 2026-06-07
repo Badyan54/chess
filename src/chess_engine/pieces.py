365f0f70a2ca9870553411057338b3309949535d
badyan <bogdanwov@gmail.com>
2026-06-07 14:06:39 +0300

    create base class for 'Q R B' and derived classes

--- src/chess_engine/pieces.py
+++ src/chess_engine/pieces.py
@@ -37,3 +37,37 @@ class _Piece:
 
     def get_combat_moves(self, board):
         return self.get_moves(board)
+
+class _SlidingPiece(_Piece):
+    """class for Queen, Rook, Bishop. their movemnent very similar"""
+    direction = ()
+
+    def get_moves(self, board):
+        moves = []
+
+        for d_x, d_y in self.direction:
+            cur_x = self.x + d_x
+            cur_y = self.y + d_y
+
+            while 0 <= cur_x <= 7 and 0 <= cur_y <= 7:
+                piece = board[cur_x][cur_y]
+                if piece == None:
+                    moves.append(Move(self.x, self.y, cur_x, cur_y, Move_type.NORMAL))
+                else:
+                    if not self.is_ally(piece.color):
+                        moves.append(Move(self.x, self.y, cur_x, cur_y, Move_type.NORMAL))
+                    break
+                cur_x += d_x
+                cur_y += d_y
+        
+        return moves 
+
+class Rook(_SlidingPiece):
+    direction = ((0, 1), (1, 0), (0, -1), (-1, 0)) #right, down, left, up
+    
+class Bishop(_SlidingPiece):
+    direction = ((1, 1), (1, -1), (-1, 1), (-1, -1)) # down-right, down-left, up-right, up-left
+        
+class Queen(_SlidingPiece):
+    direction = ((0, 1), (1, 0), (0, -1), (-1, 0), 
+                (1, 1), (1, -1), (-1, 1), (-1, -1))
